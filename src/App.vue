799094a733a0c7bc5160d9cb6bb5c30210ee5408
Luiz Guilherme <picorelli.luiz@gmail.com>
2022-07-26 03:47:01 -0300

    Add main vue files

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <TakeHome/>
+  </div>
+</template>
+
+<script>
+import TakeHome from './components/TakeHome'
+export default {
+  name: 'App',
+  components: { TakeHome },
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+html,
+body,
+#app {
+  height: 100%;
+}
+
+body {
+  font-family: 'Roboto', sans-serif;
+  margin: 0;
+}
+
+#app {
+  background: url(assets/background.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
